198c50f94bf3968634b694b0b1cbf1a141609859
MSI\tomza <tomzaioua@gmail.com>
2021-05-17 17:34:11 +0200

    modif

---
+++ Backend/app.js
@@ -0,0 +1,34 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes  = require('./routes/user');
+
+
+const path = require('path');
+
+
+
+mongoose.connect('mongodb+srv://tomAdmin:222569@cluster0.xgp6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
