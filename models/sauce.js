cd22d8baf9495b0cd5c2dcc10744ad1b3a5a5d80
Zaioua-Tom <75025858+Zaioua-Tom@users.noreply.github.com>
2021-05-17 17:22:27 +0200

    Add files via upload

---
+++ models/sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+  name         : { type: String, required: true },
+  manufacturer : { type: String, required: true },
+  description  : { type: String, required: true },
+  mainPepper   : { type: String, required: true },
+  heat         : { type: Number, required: true },
+  likes        : { type: Number},
+  dislikes     : { type: Number},
+  usersLiked   : { type: [String] },
+  usersDisliked: { type: [String] },
+  imageUrl     : { type: String, required: true },
+  userId       : { type: String, required: true } 
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
